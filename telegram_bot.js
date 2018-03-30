b304482d56bfec88676239555a50c9605236ca96
massic74 <37692200+massic74@users.noreply.github.com>
2018-03-30 15:29:03 +0200

    Add files via upload

--- telegram_bot.js
+++ telegram_bot.js
@@ -140,7 +140,7 @@ bot.on('text', msg => {
 
 function displayFeedRSS(msg_from_id){
   let parser = new Parser();
-  let feed = await parser.parseURL('view-source:http://www.fsnews.it/cms/v/index.jsp?vgnextoid=645968ae9d50a110VgnVCM10000080a3e90aRCRD');
+  let feed = parser.parseURL('view-source:http://www.fsnews.it/cms/v/index.jsp?vgnextoid=645968ae9d50a110VgnVCM10000080a3e90aRCRD');
   feed.items.forEach(item => {
       //console.log(item.title + ':' + item.link)
        bot.sendMessage(msg_from_id, item.title + ':' + item.link);
