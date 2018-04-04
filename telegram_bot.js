9f1649692df7a0ef47acdf5a4d9cc1bc4b0bbd7d
massic74 <37692200+massic74@users.noreply.github.com>
2018-04-04 12:58:24 +0200

    Add files via upload

--- telegram_bot.js
+++ telegram_bot.js
@@ -79,8 +79,8 @@ bot.on('/panorama', msg => {
 
 bot.on('photo', msg => {
     var picFile = '';
-    console.log('Foto: ' + msg.document.file_id)
-    var fotoGetPath = 'https://api.telegram.org/bot'+ token +'/getFile?file_id=' + msg.document.file_id;
+    console.log('Foto: ' + msg.photo[0].file_id)
+    var fotoGetPath = 'https://api.telegram.org/bot'+ token +'/getFile?file_id=' + msg.photo[0].file_id;
     //console.log(uricemia);
     request({ uri: fotoGetPath}, function(err, response, body){
         var bodyJSON = JSON.parse(body);
