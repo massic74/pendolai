13c056b63d03d9a2c75cafb08d7b7410cb3d5ef5
massic74 <37692200+massic74@users.noreply.github.com>
2018-04-12 15:39:59 +0200

    Add files via upload

--- telegram_bot.js
+++ telegram_bot.js
@@ -274,7 +274,7 @@ function ritardoMedio(msg){
 
      });
      console.log('MEDIA RITARDO: ' + Math.round(sommaRitardi/contra * 10) / 10);
-     bot.sendMessage(msg.from.id, 'Dalla base dati che avete generato con le vostre query risulta che la media ritardi è di minuti: ' + Math.round(sommaRitardi/contra * 10) / 10) ;
+     bot.sendMessage(msg.from.id, emoji.emojify(':red_circle:') + emoji.emojify(':red_circle:') + 'Dalla base dati che avete generato con le vostre query risulta che la media ritardi è di minuti: ' + Math.round(sommaRitardi/contra * 10) / 10) ;
      sendGifByRitardo('ritardo', msg);
   });
 
