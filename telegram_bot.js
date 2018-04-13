cba089a602dacd9ee3956277ee868904e8daed78
massic74 <37692200+massic74@users.noreply.github.com>
2018-04-13 15:39:22 +0200

    Add files via upload

--- telegram_bot.js
+++ telegram_bot.js
@@ -214,9 +214,9 @@ function getRitardoStazioni(msg){
                                               }
 
                                               messaggio =  messaggio + firstBlank + bullet + ' ' + '*' + stazioniArr[i].stazione + '*' + ' -programmata: ' + hoursProg +  ' -effettiva: '+ hoursEff  + labelRit + minRitardo + ' \n ' + '      ';
-                                              console.log('tsProgNew: ' + tsProgNew + ' - now:'  + new Date().getTime())
-                                              console.log('tsProgNew: ' + new Date(tsProgNew).toString() + ' - now:'  + new Date().toString())
-                                              if(tsProgNew > new Date().getTime()){
+                                      //        console.log('tsProgNew: ' + tsProgNew + ' - now:'  + new Date().getTime())
+                                    //          console.log('tsProgNew: ' + new Date(tsProgNew).toString() + ' - now:'  + new Date().toString())
+                                              if(tsProgNew > new Date().getTime() + (2 * 60 * 60 * 1000)){
                                                 messaggio = messaggio + '    _non ancora partito_';
                                                 break;
                                               }
